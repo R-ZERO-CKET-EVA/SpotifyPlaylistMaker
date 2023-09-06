6fc5cb029c43b0b009d037337fe7d767dd15f0a3
r-zero-cket-eva <r-zero-cket-eva@gmail.com>
2023-09-06 10:36:16 -0700

    Added try: functionality to search_song_uri

--- YTMusic/to_spotify/playlist_creator.py
+++ YTMusic/to_spotify/playlist_creator.py
@@ -1,3 +1,4 @@
+#%%
 import time
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
@@ -6,7 +7,7 @@ from config_loader import ConfigLoader
 from logger import Logger
 import re
 
-scope = "playlist-modify-public playlist-modify-private"
+scope = "playlist-modify-public playlist-modify-private user-library-read"
 
 class PlaylistCreator:
     def __init__(self, config_file: str):
@@ -51,7 +52,10 @@ class PlaylistCreator:
             track_cleaned = re.sub(r' \(feat\..*\)', '', track)
             
             # First, search using artist and track
-            results = self.sp.search(q=f"artist:{artist} track:{track_cleaned}", type="track", limit=10)
+            if artist == "Manual Query: ":
+                results = self.sp.search(q=f"{track}", type="track", limit=10)
+            else:
+                results = self.sp.search(q=f"artist:{artist} track:{track_cleaned}", type="track", limit=10)
             tracks = results["tracks"]["items"]
             
             # If no tracks were found, search again using just the track
@@ -65,12 +69,15 @@ class PlaylistCreator:
                 return None
             
             # If the first result matches the artist and title perfectly, auto-select it
-            if tracks[0]['artists'][0]['name'].lower() == artist.lower() and tracks[0]['name'].lower() == track_cleaned .lower():
+            if tracks[0]['artists'][0]['name'].lower() == artist.lower() and tracks[0]['name'].lower() == track_cleaned.lower():
                 print(f"Automatically added {artist} - {track_cleaned} to the playlist.")
                 return tracks[0]["uri"]
             
             # Display the searched artist and track
-            print(f"Searching for: {artist} - {track_cleaned}\n")
+            if artist == "Manual Query: ":
+                print(f"Searching for: {track}\n")
+            else:
+                print(f"Searching for: {artist} - {track_cleaned}\n")
             
             # Display the top 10 results
             for idx, track_item in enumerate(tracks, 1):
@@ -80,7 +87,7 @@ class PlaylistCreator:
             
             # Prompt the user to select a song or skip
             while True:
-                choice = input("Select a song (1-10) or type 's' to skip: ")
+                choice = input("Select a song (1-10), type 's' to skip, or 'try: query' to manually search your query: ")
                 
                 if choice.isdigit() and 1 <= int(choice) <= 10:
                     selected_track = tracks[int(choice) - 1]
@@ -88,8 +95,11 @@ class PlaylistCreator:
                 elif choice.lower() in ['s', 'skip']:
                     print("Skipping track...")
                     return None
+                elif choice.lower()[:4] == "try:":
+                    print('Searching manual query')
+                    return self.search_song_uri("Manual Query: ", choice.lower()[5:])
                 else:
-                    print("Invalid choice. Please select a number between 1 and 10 or type 's' to skip.")
+                    print("Invalid choice. Please select a number between 1 and 10, type 's' to skip, or type 's' to search just the track name")
         
         except Exception as e:
             Logger("error.log").log_error(str(e))
@@ -101,3 +111,24 @@ class PlaylistCreator:
         Handles rate limit by waiting for the appropriate time before making the next request.
         """
         time.sleep(0.5)  # Wait for 0.5 seconds before making the next request
+
+    def get_liked_songs(self):
+        offset = 0
+        limit = 50  # Maximum allowed by the API per call
+        all_liked_songs = []
+
+        while True:
+            results = self.sp.current_user_saved_tracks(limit=limit, offset=offset)
+            if not results["items"]:
+                break
+            
+            for item in results["items"]:
+                track_info = {
+                    "uri": item["track"]["uri"],
+                    "publishedAt": item["added_at"]
+                }
+                all_liked_songs.append(track_info)
+            
+            offset += limit
+
+        return all_liked_songs
